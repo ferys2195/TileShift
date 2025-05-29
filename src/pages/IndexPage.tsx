03c06cdbaaa156c7934c3c335280aa7124c6efa8
ferys2195 <fery21irawan.me@gmail.com>
2025-05-29 14:06:19 +0700

    rename game name

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -3,10 +3,10 @@ import Puzzle from "../components/Puzzle";
 
 export default function IndexPage() {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex h-screen flex-col overflow-hidden p-3">
       <header className="p-3">
         <div className="text-center">
-          <h1 className="mb-1 text-2xl font-bold">Box Puzzle Game</h1>
+          <h1 className="mb-1 text-2xl font-bold">TileShift Game</h1>
         </div>
       </header>
       <div className="container mx-auto flex-1">
